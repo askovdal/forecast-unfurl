febc600471425e43c98225b3075fefb2cfd711e1
Andreas Skovdal <andreasskovdal@gmail.com>
2020-06-23 13:08:32 +0200

    Add initial express app

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import enforce from 'express-sslify';
+
+import { IN_PROD, PORT, VERIFICATION_TOKEN } from './config.js';
+
+const app = express();
+IN_PROD && app.use(enforce.HTTPS());
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  const { body } = req;
+
+  if (body.type === 'url_verification' && body.token === VERIFICATION_TOKEN) {
+    res.json({ challenge: body.challenge });
+  }
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
