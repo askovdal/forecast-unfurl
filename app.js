a0c5ec89a8b5435624b41ac76cc377e89cd2879f
askovdal <andreasskovdal@gmail.com>
2020-06-25 12:49:45 +0200

    Get forecast task name and add it to the unfurl

--- app.js
+++ app.js
@@ -2,13 +2,71 @@ import axios from 'axios';
 import express from 'express';
 import enforce from 'express-sslify';
 
-import { IN_PROD, OAUTH_TOKEN, PORT, VERIFICATION_TOKEN } from './config.js';
+import {
+  FORECAST_API_KEY,
+  IN_PROD,
+  OAUTH_TOKEN,
+  PORT,
+  VERIFICATION_TOKEN,
+} from './config.js';
 
 const app = express();
 IN_PROD && app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.json());
 
-app.post('/', (req, res) => {
+const getTask = async (id) => {
+  const response = await axios({
+    url: `https://api.forecast.it/api/v2/tasks/company_task_id/${id}`,
+    method: 'get',
+    headers: { 'X-FORECAST-API-KEY': FORECAST_API_KEY },
+  }).catch((_reason) => {});
+
+  if (!response) return;
+
+  return response.data;
+};
+
+const createUnfurls = async (event) => {
+  const unfurls = {};
+  for (const { url } of event.links) {
+    const id = url.match(/\/T(\d+)$/);
+    if (!id) continue;
+
+    const task = await getTask(id[1]);
+    if (!task) continue;
+
+    unfurls[url] = {
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `<${url}|T${id[1]}: ${task.title}>`,
+          },
+        },
+      ],
+    };
+  }
+
+  return unfurls;
+};
+
+const unfurlMessage = (event, unfurls) => {
+  axios({
+    url: 'https://slack.com/api/chat.unfurl',
+    method: 'post',
+    headers: { Authorization: `Bearer ${OAUTH_TOKEN}` },
+    data: {
+      channel: event.channel,
+      ts: event.message_ts,
+      unfurls: unfurls,
+    },
+  })
+    .then(({ data }) => console.log(data))
+    .catch(console.error);
+};
+
+app.post('/', async (req, res) => {
   const { body } = req;
 
   if (body.token !== VERIFICATION_TOKEN) {
@@ -22,38 +80,8 @@ app.post('/', (req, res) => {
   res.status(200).end();
 
   const { event } = body;
-
-  const unfurls = event.links.reduce((unfurls, { url }) => {
-    const id = url.match(/\/(T\d+)$/);
-    if (!id) return unfurls;
-
-    unfurls[url] = {
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `<${url}|${id[1]}: Task name>`,
-          },
-        },
-      ],
-    };
-
-    return unfurls;
-  }, {});
-
-  axios({
-    url: 'https://slack.com/api/chat.unfurl',
-    method: 'post',
-    headers: { Authorization: `Bearer ${OAUTH_TOKEN}` },
-    data: {
-      channel: event.channel,
-      ts: event.message_ts,
-      unfurls: unfurls,
-    },
-  })
-    .then(({ data }) => console.log(data))
-    .catch(console.error);
+  const unfurls = await createUnfurls(event);
+  unfurlMessage(event, unfurls);
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
