844b3eb9480e5429610398f20b43d69c12ecf0e1
Andreas Skovdal <andreasskovdal@gmail.com>
2020-07-29 16:03:44 +0200

    Replace tab characters with spaces

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ const createUnfurls = async ({ links }) => {
     }
 
     // Build context text and replace normal spaces with non-breaking spaces
-    const contextText = `Status: *${status}*\t\t${assigneeText}\t\tRole: *${role}*`
+    const contextText = `Status: *${status}*        ${assigneeText}        Role: *${role}*`
       .split(' ')
       .join(' ');
 
