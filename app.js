d1c96bc434bd19bfcc6a6dc8b0538a1d2ad52611
Andreas Skovdal <andreasskovdal@gmail.com>
2020-07-29 16:06:35 +0200

    Revert "Remove nbsps for testing"
    This reverts commit fa1d7fec2aed6dd0bac3f2761322cc69def291a2.

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ const createUnfurls = async ({ links }) => {
     // Build context text and replace normal spaces with non-breaking spaces
     const contextText = `Status: *${status}*        ${assigneeText}        Role: *${role}*`
       .split(' ')
-      .join(' ');
+      .join(' ');
 
     unfurls[url] = {
       color: '#6e0fea',
