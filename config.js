a3436612bac0f40758c013213399eb9713d98837
askovdal <andreasskovdal@gmail.com>
2020-09-01 14:28:30 +0200

    Use camelize

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const {
   NODE_ENV = 'development',
   OAUTH_TOKEN,
   PORT = 3001,
-  VERIFICATION_TOKEN = 'test_token',
+  VERIFICATION_TOKEN = 'testToken',
 } = process.env;
 
 export const IN_PROD = NODE_ENV === 'production';
